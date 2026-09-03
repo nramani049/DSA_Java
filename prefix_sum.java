2f0df35844b692d394026365214f57aca2980838
nramani049 <nramani049@users.noreply.github.com>
2026-09-03 10:38:40 +0530

    Refactor prefix_sum.java to improve array handling and output formatting

--- prefix_sum.java
+++ prefix_sum.java
@@ -6,6 +6,22 @@ class prefix_sum{
         System.out.println("Enter array size:");
         int n=sc.nextInt();
 
+        int[] arr= new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
         
+        int[] prefix_sum=new int[n];
+        prefix_sum[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefix_sum[i]=prefix_sum[i-1]+arr[i];
+        }
+
+        System.out.println("Prefix sum array:");
+        for(int i=0;i<n;i++){
+            System.out.print(prefix_sum[i]+" ");
+        }
+        sc.close();
     }
 }
